3e9f67c587297ebda159d21cd08d1fed9d56693a
edmond120 <edmondwong888@msn.com>
2017-12-02 21:24:44 -0500

    made dqueue

---
+++ data_structures/dqueue.h
@@ -0,0 +1,21 @@
+struct input_dqueue{ //the input_dqueue's job is to store a history of inputs
+	char ** dqueue;
+	int size;
+	int start;
+	int end;
+};
+
+//creates and inits an input_dqueue with size n and returns a pointer to it
+struct input_dqueue * input_dqueue_init(int n);
+
+//frees the dequeue
+void free_input_dqueue(struct input_dqueue *);
+
+//frees the end of the dequeue
+void free_input_dqueue_end(struct input_dqueue *);
+
+//adds to the start of the dequeue
+void push_input_dqueue(char * input, struct input_dqueue *);
+
+//prints the dqueue
+void print_input_dqueue(struct input_dqueue *);
