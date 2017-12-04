6c0082325cdebfeb75d8490e9f816da39115664e
edmond120 <edmondwong888@msn.com>
2017-12-04 11:45:11 -0500

    started on cursor move functions

--- main.c
+++ main.c
@@ -4,11 +4,23 @@
 #include  <sys/types.h>
 #include  <ncurses.h>
 
-//void move_back
+void move_back(WINDOW * win,int spaces){
+	int y,x;
+	getyx(win,y,x);
+	move(y,x - spaces);
+}
+void move_foward(WINDOW * win,int spaces){
+
+}
 
 int main(){
 	input_init(20,10);
-	
+	move(0,10);
+	move_foward(stdscr,5);
+	getch();
+	move_back(stdscr,5);
+	refresh();
+	getch();
 	endwin();
 	return 0;
 }
